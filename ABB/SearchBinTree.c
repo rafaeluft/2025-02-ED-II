1d63e95b2b27cc0b92150c1e7cf8e7982942a109
pprafaellima <pp.rafaellima@gmail.com>
2025-09-04 16:50:32 -0300

    Remoção da ABB

--- ABB/SearchBinTree.c
+++ ABB/SearchBinTree.c
@@ -38,7 +38,12 @@ bool BinTree_insert(BinTree* T, int key){
             y->right = z;
     return true;
 }
-TNo* BinTree_search(TNo*, int);
+TNo* BinTree_search(TNo* root, int search_key){
+    if(root == NULL || root->key == search_key)
+        return root;
+    else
+        return BinTree_search((search_key < root->key)?root->left:root->right, search_key);
+}
 void BinTree_pre(TNo* root){
     if(root){
         printf("%d, ", root->key);
@@ -66,4 +71,46 @@ void BinTree_pos(TNo* root){
             BinTree_pos(root->right);
         printf("%d, ", root->key);
     }    
+}
+
+void Transplant(BinTree* T, TNo* u, TNo* v){
+    if (u->p == NULL)
+        T->root = v;
+    else 
+        if(u==u->p->left)
+            u->p->left = v;
+        else
+            u->p->right = v;
+    if (v!= NULL)
+        v->p = u->p;
+}
+TNo* Tree_minimum(TNo* x){
+    while (x->left != NULL)
+        x = x->left;
+    return x;
+}
+
+bool BinTree_delete(BinTree* T, int key){
+    TNo* z = BinTree_search(T->root, key);
+    if(z==NULL)
+        return false;
+    if (z->left == NULL)
+        Transplant(T, z, z->right);
+    else
+        if(z->right == NULL)
+            Transplant(T, z, z->left);
+        else
+        {
+            TNo* y = Tree_minimum(z->right);
+            if(y->p != z){
+                Transplant(T, y, y->right);
+                y->right = z->right;
+                y->right->p = y;
+            }
+            Transplant(T, z, y);
+            y->left = z->left;
+            y->left->p = y;
+        }
+    free(z);
+    return true;
 }
--- ABB/main.c
+++ ABB/main.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
 #include "SearchBinTree.h"
 
+
+
+
 void PrintTree(BinTree *T){
     BinTree_pre(T->root);
     putchar('\n');
@@ -13,6 +16,15 @@ int main(){
     for(int i=0; i< 7; i++)
         if(!BinTree_insert(T, V[i]))
             printf("Erro ao inserir o elemento V[%i] = %i\n", i, V[i]);
+    PrintTree(T);
+    int to_be_deleted;
+    //int to_be_deleted = 25;
+    //BinTree_delete(T, to_be_deleted);
+    to_be_deleted = 10;
+    if(BinTree_delete(T, to_be_deleted))
+        printf("No %d deletado com sucesso!\n", to_be_deleted);
+    else
+        printf("Nao encontrei no com o rotulo %d\n", to_be_deleted);
     PrintTree(T);
     return 0;
 }
